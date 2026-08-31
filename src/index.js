9ae4d3bfb210a3646777f56920daa42c1ee301a3
AnthonyDLC <277806914+anthony-dlc@users.noreply.github.com>
2026-08-31 12:05:13 -0400

    Eliminate task

--- src/index.js
+++ src/index.js
@@ -31,6 +31,8 @@
 
 // ############################# LOGICA NUEVA ##############################
 
+// localStorage.setItem("projects", JSON.stringify([]))
+
 import createNewProject from "./project/createProject.js"; // Project Crud
 import readProject from "./project/readProject.js"; // Project Crud
 import editProjectName from "./project/editProject.js"; // Project Crud
@@ -39,6 +41,21 @@ import eliminateProject from "./project/eliminateProject.js"; // Project Crud
 import createNewTask from "./task/createTask.js"; // Task Crud
 import readTask from "./task/readTask.js"; // Task Crud
 import editTask from "./task/editTask.js"; // Task Crud
+import eliminateTask from "./task/eliminateTask.js"; // Task Crud
 
 import uploadElement from "./storage/uploadProject.js"; // Storage
 import getElement from "./storage/getElement.js"; // Storage
+
+
+
+// eliminateTask(
+// "bc4857c5-f46b-46de-a582-798e12aea521","cc691a84-0f32-4575-b9ec-ebf00a23cad8")
+
+const task01 = ["Homework","Do Homework","23/08","High","Not done"];
+const task02 = ["Workout","Functional workout","23/08","High","Done"];
+
+
+// createNewTask("d04f4cfb-de71-4f0d-9f8f-c40e9479b184",task02)
+
+
+
--- src/task/eliminateTask.js
+++ src/task/eliminateTask.js
@@ -1,13 +1,37 @@
+// - Recibir id del proyecto y id de la tarea.
+// - LLamar Array desde localStorage.
+// - Obtener indice del proyecto dentro del array.
+// - Si aparece:
+//   - Buscar indice del id de la tarea dentro del proyecto.
+//   - Si aparece:
+//     - Eliminar tarea del proyecto.
+//     - Agregar Array a localStorage.
+//   - Sino:
+//     - Mostrar tarea no encontrada.
+// - Sino:
+//   - Mostrar proyecto no encontrado.
+
 import getElement from "../storage/getElement.js";
+import getProjectIndex from "../storage/projectIndex.js";
 import uploadElement from "../storage/uploadProject.js";
 
 export default function eliminateTask(projectId, taskId) {
-  const project = getElement(projectId);
-  const indexTask = project.taskList.findIndex((el) => el.id === taskId);
-  if (indexTask !== -1) {
-    project.taskList.splice(indexTask, 1);
-    uploadElement(project);
-    return "Task Eliminated";
+  const projectContainer = getElement("projects");
+  const projectIndex = getProjectIndex(projectContainer, projectId);
+  if (projectIndex !== -1) {
+    const taskIndex = getProjectIndex(
+      projectContainer[projectIndex].taskList,
+      taskId,
+    );
+    if (taskIndex !== -1) {
+      projectContainer[projectIndex].taskList.splice(taskIndex, 1);
+      uploadElement(projectContainer)
+      return;
+    }
+    console.log("Task not found");
+    return;
   }
-  return "Task Not found";
+  console.log("Project not found");
+  return;
 }
+
